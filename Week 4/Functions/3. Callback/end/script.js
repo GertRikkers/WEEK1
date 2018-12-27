5d1f39fe35164b9609216c71b2ff7bcc962c869e
Gert Rikkers <g.rikkers@hccnet.nl>
2018-12-27 17:34:31 +0100

    Starting with Callback functions

---
+++ Week 4/Functions/3. Callback/end/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var ES5_ShowTemperature = function() {
+    main.showResult("ES5: Buiten is het 30 graden celcius.");
+}
+
+const ES6_ShowTemperature = () => {
+    main.showResult("ES6: Buiten is het 30 graden celcius.");
+}
+
+var ES5_ShowTemperatureWithParam = function(celcius) {
+    main.showResult("ES5: Buiten is het " + celcius + " graden celcius.");
+}
+
+const ES6_ShowTemperatureWithParam = (celcius) => {
+    main.showResult("ES6: Buiten is het " + celcius + " graden celcius.");
+}
+
+var ES5_ShowTemperatureWithParamAndReturn = function(celcius) {
+    return "ES5: Buiten is het " + celcius + " graden celcius.";
+}
+
+const ES6_ShowTemperatureWithParamAndReturn = (celcius) => {
+    return "ES6: Buiten is het " + celcius + " graden celcius.";
+}
+
+main.showTitle("Functions zonder parameter")
+ES5_ShowTemperature();
+ES6_ShowTemperature();
+main.showTitle("Functions met parameter")
+ES5_ShowTemperatureWithParam(31);
+ES6_ShowTemperatureWithParam(31);
+main.showTitle("Functions met parameter en return waarde")
+var ES5_result = ES5_ShowTemperatureWithParamAndReturn(32);
+main.showResult(ES5_result);
+const ES6_result = ES6_ShowTemperatureWithParamAndReturn(32);
+main.showResult(ES6_result);
---
+++ Week 4/Functions/3. Callback/readme.md
@@ -0,0 +1,32 @@
+## Functions - Callback
+
+### Doel
+Het leren gebruiken van een Callback Function.
+1. Je kunt een Callback Function maken.
+2. Je kunt uitleggen wat een Callback function is.
+
+### Opdracht
+1. Maak een ES5 Function zonder parameters.
+2. Zet deze Function om naar een Arrow Function.
+3. Maak een ES5 Function met parameters.
+4. Zet deze Function om naar een Arrow Function. 
+5. Maak een ES5 Function met parameters en een return.
+6. Zet deze Function om naar een Arrow Function. 
+7. Noem twee belangrijke redenen om Arrow Fucntions te gebruiken.
+
+### Kennis
+Het maken en gebruiken van een Arrow Function en het omzetten van een ES5 Function naar een Arrow Function.  
+```
+// ES5
+var f = function(){}
+var f = function(parameter){}
+var f = function(parameter){return "Return Parameter"}
+
+// ES6
+const f = () => {}
+const f = (parameter) => {}
+const f = (parameter) => "Return Parameter"
+```
+
+### Opmerking 
+Zet de code in een apart JavaScript bestand.
